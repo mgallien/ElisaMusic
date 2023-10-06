e097ef3fd4842ccbc545195c43834e96424eb8d6
Matthieu Gallien <matthieu_gallien@yahoo.fr>
2023-10-06 23:17:34 +0200

    initial commit

--- lib/Controller/PageController.php
+++ lib/Controller/PageController.php
@@ -1,6 +1,7 @@
 <?php
-
 declare(strict_types=1);
+// SPDX-FileCopyrightText: Matthieu Gallien <matthieu_gallien@yahoo.fr>
+// SPDX-License-Identifier: AGPL-3.0-or-later
 
 namespace OCA\Elisa\Controller;
 
--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,8 @@
 <template>
+        <!--
+        SPDX-FileCopyrightText: Matthieu Gallien <matthieu_gallien@yahoo.fr>
+        SPDX-License-Identifier: AGPL-3.0-or-later
+        -->
 	<NcAppContent>
 		<div id="elisa">
 			<h1>Hello world!</h1>
