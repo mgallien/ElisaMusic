89f32cf6c37dc494d93c3b7ffb1554ca4cbbdc46
Matthieu Gallien <matthieu_gallien@yahoo.fr>
2024-06-01 17:06:09 +0200

    fix syntax of reuse SPDX copyright comments

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -1,9 +1,9 @@
-// SPDX-FileCopyrightText: Matthieu Gallien <matthieu_gallien@yahoo.fr>
-// SPDX-License-Identifier: AGPL-3.0-or-later
-
 <?php
 
 declare(strict_types=1);
+
+// SPDX-FileCopyrightText: Matthieu Gallien <matthieu_gallien@yahoo.fr>
+// SPDX-License-Identifier: AGPL-3.0-or-later
 
 namespace OCA\Elisa\AppInfo;
 
