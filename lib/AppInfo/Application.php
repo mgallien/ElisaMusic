8bbf6d71251d299afd27f4f33d9b3dd3bb0d47fd
Matthieu Gallien <matthieu_gallien@yahoo.fr>
2024-06-22 16:49:45 +0200

    remove listeners for metadata: not needed

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -7,16 +7,11 @@ declare(strict_types=1);
 
 namespace OCA\Elisa\AppInfo;
 
-use OCA\Elisa\Listeners\UpdateFilesMetadata;
 use OCA\Elisa\Hooks\ElisaFilesHooksStatic;
 use OCP\AppFramework\App;
 use OCP\AppFramework\Bootstrap\IBootContext;
 use OCP\AppFramework\Bootstrap\IBootstrap;
 use OCP\AppFramework\Bootstrap\IRegistrationContext;
-use OCP\FilesMetadata\Event\MetadataBackgroundEvent;
-use OCP\FilesMetadata\Event\MetadataLiveEvent;
-use OCP\FilesMetadata\IFilesMetadataManager;
-use OCP\FilesMetadata\Model\IMetadataValueWrapper;
 use OCP\Util;
 
 class Application extends App implements IBootstrap {
@@ -33,9 +28,6 @@ class Application extends App implements IBootstrap {
 		Util::connectHook('OC_Filesystem', 'delete', ElisaFilesHooksStatic::class, 'fileDelete');
 		Util::connectHook('OC_Filesystem', 'rename', ElisaFilesHooksStatic::class, 'fileMove');
 		Util::connectHook('OC_Filesystem', 'post_rename', ElisaFilesHooksStatic::class, 'fileMovePost');
-
-		$context->registerEventListener(MetadataLiveEvent::class, UpdateFilesMetadata::class);
-		$context->registerEventListener(MetadataBackgroundEvent::class, UpdateFilesMetadata::class);
 	}
 
 	public function boot(IBootContext $context): void {
